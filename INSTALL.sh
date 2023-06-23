59b8ff82afb96279fc02e58793bda1ddd97c70ba
Joshua Silver <jsilver@uow.edu.au>
2023-06-23 17:30:49 +1000

    Automated a few install steps

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SCRIPT_NAME="TransferScript"
+USER_NAME="user"
 INSTALL_PATH="/usr/local/scripts/${SCRIPT_NAME}"
 SYSTEMD_PATH="/etc/systemd/system"
 
@@ -14,6 +15,13 @@ cp excludes.txt ${INSTALL_PATH}/
 touch ${INSTALL_PATH}/rclone.conf
 cp TransferScript.service ${SYSTEMD_PATH}/${SCRIPT_NAME}.service
 cp TransferScript.timer ${SYSTEMD_PATH}/${SCRIPT_NAME}.timer
+
+# Update permissions
+chown -R ${USER_NAME} ${INSTALL_PATH}
+
+# Update service file
+sed -i 's|^User=.*|User=${USER_NAME}|g' ${SYSTEMD_PATH}/${SCRIPT_NAME}.service
+sed -i 's|^ExecStart=.*|ExecStart=/usr/bin/python3 -O ${INSTALL_PATH}/${SCRIPT_NAME}.py|g' ${SYSTEMD_PATH}/${SCRIPT_NAME}.service
 
 # Ensure Permissions
 chmod 755 ${INSTALL_PATH}/${SCRIPT_NAME}.py
