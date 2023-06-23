8fc610cefd0c2b674e1f50b0b55faaa62c37c76c
Joshua Silver <jsilver@uow.edu.au>
2023-06-23 18:36:38 +1000

    Automated a few install steps

--- INSTALL.sh
+++ INSTALL.sh
@@ -11,7 +11,7 @@ mkdir -p ${INSTALL_PATH}/logs
 # Copy files to respective locations
 cp TransferScript.py ${INSTALL_PATH}/${SCRIPT_NAME}.py
 cp config.json ${INSTALL_PATH}/
-cp excludes.txt ${INSTALL_PATH}/
+cp filters.txt ${INSTALL_PATH}/
 touch ${INSTALL_PATH}/rclone.conf
 cp TransferScript.service ${SYSTEMD_PATH}/${SCRIPT_NAME}.service
 cp TransferScript.timer ${SYSTEMD_PATH}/${SCRIPT_NAME}.timer
@@ -20,8 +20,8 @@ cp TransferScript.timer ${SYSTEMD_PATH}/${SCRIPT_NAME}.timer
 chown -R ${USER_NAME} ${INSTALL_PATH}
 
 # Update service file
-sed -i 's|^User=.*|User=${USER_NAME}|g' ${SYSTEMD_PATH}/${SCRIPT_NAME}.service
-sed -i 's|^ExecStart=.*|ExecStart=/usr/bin/python3 -O ${INSTALL_PATH}/${SCRIPT_NAME}.py|g' ${SYSTEMD_PATH}/${SCRIPT_NAME}.service
+sed -i "s|^User=.*|User=${USER_NAME}|g" ${SYSTEMD_PATH}/${SCRIPT_NAME}.service
+sed -i "s|^ExecStart=.*|ExecStart=/usr/bin/python3 -O ${INSTALL_PATH}/${SCRIPT_NAME}.py|g" ${SYSTEMD_PATH}/${SCRIPT_NAME}.service
 
 # Ensure Permissions
 chmod 755 ${INSTALL_PATH}/${SCRIPT_NAME}.py
